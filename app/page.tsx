3439ef5f1fdd8fcd831ad9aad1b4d8b4e71bd8d0
Mbas <mbas750@gmail.com>
2026-01-27 19:47:04 +0100

    scrolltrigger basics

--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,8 @@ const Home = () => {
   return (
     <>
      <Hero />
-     <SomeSection />
      <Footer />
+     <SomeSection />
     </>
   )
 }
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,33 @@
-import React from 'react'
+"use client"
+
+import gsap from 'gsap'
+import { useGSAP } from '@gsap/react'
+import { useRef } from 'react'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+
+gsap.registerPlugin(ScrollTrigger)
 
 const Footer = () => {
+    const cardRef = useRef<HTMLDivElement>(null);
+    useGSAP(() => {
+      gsap.to(cardRef.current, {
+        scrollTrigger: {
+          trigger: cardRef.current,
+          toggleActions: "restart pause reverse none",
+          scrub: 2,
+          markers: true
+        },
+        x: 500,
+        rotation: 360,
+        duration: 3,
+        ease: "none"
+      })
+  });
   return (
-    <footer className='h-svh bg-[#080416] text-white flex items-center justify-center'>
-        <h1 className='text-4xl font-semibold'>Footer</h1>
+    <footer className='h-svh bg-[#361f03] flex justify-start'>
+        <div className=''>
+            <div className='w-15 h-15 bg-white rounded-lg' ref={cardRef}></div>
+        </div>
     </footer>
   )
 }
